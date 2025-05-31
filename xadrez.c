310b9b25050a8802d0694ee21580513c6608635e
Daniel Cavalli <leaodrj@gmail.com>
2025-05-31 21:20:16 +0000

    Biblioteca necessária para entrada e saída de dados

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <iostream> // Biblioteca necessária para entrada e saída de dados
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
